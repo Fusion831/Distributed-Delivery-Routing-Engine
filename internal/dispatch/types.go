aebbcdecb8cab7385897d0b8a20cd6dd13ae4265
Daksh <dakshdaksh698@gmail.com>
2026-01-15 11:19:09 +0530

    Added two types of structs, to handle The HTTP Routing Result as well being able to cancel when the User closes the app passing context, and utilizing channels for CSP, Need to look into buffered vs unbuffered channels to handle backpressure , by building a worker pool

--- internal/dispatch/types.go
+++ internal/dispatch/types.go
@@ -1,1 +1,18 @@
 package dispatch
+
+import (
+	"context"
+
+	"github.com/Fusion831/Distributed-Delivery-Routing-Engine/pkg/model"
+)
+
+type RouteResult struct {
+	PathResult []*model.Node
+	err        error
+}
+
+type RouteRequest struct {
+	Start, End *model.Node
+	Ctx        context.Context
+	ResultChan chan *RouteResult
+}
