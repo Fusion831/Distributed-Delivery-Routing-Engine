5c3c4e3f3ddfe07da1204dbb267359a53d126ee0
Daksh <dakshdaksh698@gmail.com>
2026-01-15 21:40:17 +0530

    Initlized the Dispatcher structs with a Buffered queue and it's constructor

--- internal/dispatch/dispatcher.go
+++ internal/dispatch/dispatcher.go
@@ -1,1 +1,19 @@
 package dispatch
+
+import (
+	"github.com/Fusion831/Distributed-Delivery-Routing-Engine/pkg/model"
+)
+
+type Dispatcher struct {
+	jobQueue chan *RouteRequest
+	workers  int
+	graph    model.Graph
+}
+
+func NewDispatcher(workerCount, bufferSize int, graph model.Graph) *Dispatcher {
+	return &Dispatcher{
+		jobQueue: make(chan *RouteRequest, bufferSize),
+		workers:  workerCount,
+		graph:    graph,
+	}
+}
